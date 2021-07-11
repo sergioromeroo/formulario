91118d862426785e5dd59ef3ef613e2102af62de
sergioromeroo <manusergio96@gmail.com>
2021-07-11 19:13:55 -0300

    termino login y register

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ const path = require('path');
 const port = 3040;
 app.use(express.static("public"))
 app.get("/", (req,res) => res.sendFile(path.join(__dirname, "views", "home.html")));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
+
 
 app.listen(port, () => console.log(" en el puerto " + port));
